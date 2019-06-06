bd31fa80838434cea3c055b2bc2b8e46e3084e93
梁景雄 <743042009@qq.com>
2019-06-06 23:06:50 +0800

    加入了跨域处理

--- src/main/java/com/ruili/fota/controller/AccountController.java
+++ src/main/java/com/ruili/fota/controller/AccountController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping(value = "authority")
 public class AccountController extends BaseController {
 
