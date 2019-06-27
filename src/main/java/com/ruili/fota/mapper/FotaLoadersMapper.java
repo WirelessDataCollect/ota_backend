2346c80eb0ec63acf24de0ab4b088e759daed086
梁景雄 <743042009@qq.com>
2019-06-27 19:54:03 +0800

    update

--- src/main/java/com/ruili/fota/mapper/FotaLoadersMapper.java
+++ src/main/java/com/ruili/fota/mapper/FotaLoadersMapper.java
@@ -2,6 +2,7 @@ package com.ruili.fota.mapper;
 
 import com.ruili.fota.common.BaseMapper;
 import com.ruili.fota.meta.po.FotaLoaders;
+import org.apache.ibatis.annotations.Select;
 
 public interface FotaLoadersMapper extends BaseMapper<FotaLoaders> {
 }
--- src/test/java/com/ruili/fota/Test.java
+++ src/test/java/com/ruili/fota/Test.java
@@ -1,6 +1,8 @@
 package com.ruili.fota;
 
 import com.alibaba.fastjson.JSON;
+
+import com.google.common.collect.Maps;
 import com.ruili.fota.common.CopyTools;
 import com.ruili.fota.common.DateTools;
 import com.ruili.fota.meta.bo.ConfigBO;
@@ -12,17 +14,50 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
-
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @SpringBootTest
 public class Test {
 
-    public static void main(String[] args) throws InterruptedException {
-        String str = "{\"RecID\":23,\"SendID\":45,\"imei\":\"865192040010133\",\"cannum\":1,\"measure\":66,\"firmwareId\":\"191be9d1b1ac4ecf97b55f1b70a0228d\",\"mcuType\":\"中央控制器\",\"fotaImages\":{\"gid\":24,\"firmwareId\":\"191be9d1b1ac4ecf97b55f1b70a0228d\",\"mcuType\":\"单通道控制器\",\"fileName\":\"led_blink\",\"uploader\":\"我波哥波哥\",\"uploadertel\":\"123123\",\"firmVersion\":\"1.0.0\",\"content\":\"小文件50k\",\"gmtcreate\":\"Sun Jun 16 17:10:17 CST 2019\",\"gmtupdate\":\"Sun Jun 16 17:10:17 CST 2019\"}}";
-        ConfigBO configBO = JSON.parseObject(str, ConfigBO.class);
-        configBO.setFotaImages(null);
-        System.out.println(configBO.getFotaImages() == null);
+    public static void main() throws Exception {
+        String uri = "111"; // 参照文档中, 样例 /access/spot/saveSpotPriceStrategy
+        String key = "11";  // 找 @景鸿 要
+        String secret = "11"; // 找 @景鸿 要
+        String timeStamp = String.valueOf(System.currentTimeMillis() + 50000L);
 
+        Map<String, String> param = Maps.newHashMap();
+        param.put("timestamp", timeStamp);
+        param.put("key", key);
+        String sign =  generateSign(param, secret, uri);
+        param.put("sign", sign);
+        System.out.println(JSON.toJSONString(param));
+    }
+
+    public static String generateSign(Map<String, String> param, String secret, String uri) {
+        Set<String> keySet = param.keySet();
+        List<String> keys = new ArrayList<>(keySet);
+        keys.remove("sign");
+        Collections.sort(keys);
+
+        StringBuilder builder = new StringBuilder();
+        builder.append(uri);
+        for (String paramKey : keys) {
+            builder.append(paramKey).append(param.get(paramKey));
+        }
+        builder.append(secret);
+        String content = builder.toString();
+
+        // 验证签名
+        // try {
+        //     return SignatureAlgorithm.MD5.sign(content, SecurityUtils.hexEncoder);
+        // } catch(Exception e) {
+        //     System.out.println(e);
+        // }
+        return null;
     }
 }
